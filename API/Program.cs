980ee8feaf3113e9876bd18738006e77df370a1c
mrb <kristian.w.finstad@gmail.com>
2026-08-30 22:18:30 +0200

    Structure defined

--- API/Program.cs
+++ API/Program.cs
@@ -4,11 +4,8 @@ using Serilog;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.AddSerilogLogging();
-builder.Services.AddApplicationServices();
-builder.Services.AddDatabaseServices(builder.Configuration);
 
 var app = builder.Build();
-
 
 app.UseSerilogRequestLogging();
 
@@ -16,8 +13,6 @@ app.UseRouting();
 
 app.MapControllers();
 
-// SignalR Hub mapping - matcher rutingen fra Gateway (/hubs/*)
-// app.MapHub<RecipeHub>("/hubs/recipe");
 
 app.Logger.LogInformation("🚀 Applikasjonen har startet og lytter på forespørsler!");
 app.Run();
